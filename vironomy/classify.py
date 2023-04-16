73585e7dc323179ac6a881e4f262bf07322f6039
Braden Tierney <b-tierney@users.noreply.github.com>
2023-04-16 12:11:07 -0400

    Update classify.py

--- vironomy/classify.py
+++ vironomy/classify.py
@@ -141,7 +141,7 @@ class queryset:
 			tophits = pd.merge(tophits,self.taxmap,on='genbank_contigid',how='left').drop('number_of_occurences_of_taxonomic_annotation_per_group',axis=1)
 			tophits['found_by'] = 'Secondary clustering'
 			if (tosubcluster/count) % 10 == 0:
-				print('	Subclustering is %s%% complete.'%str(100*(count/tosubcluster)))
+				print('	Subclustering is %s%% complete.'%str(round(1000*(count/tosubcluster)),2))
 			# merge all output 
 			self.taxadb_secondary[m] = tophits
 		return(full_database) 
